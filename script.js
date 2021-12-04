960fff9673a28269e6c99847971460b623c875fc
Ahemd Sirag <a7medsr.dev@gmail.com>
2021-12-04 03:11:32 +0200

    implement game conditions

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const secertNumber = Math.trunc(Math.random() * 20) + 1;
+
+console.log(secertNumber);
+//Event Handler for check button
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  //Implement game logic
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚨 Not a valid number';
+  } else if (guess === secertNumber) {
+    document.querySelector('.message').textContent = '🏆 Correct Number ';
+  } else if (guess > secertNumber) {
+    document.querySelector('.message').textContent = '📈 Too High ';
+  } else if (guess < secertNumber) {
+    document.querySelector('.message').textContent = '📉 Too Low ';
+  }
+});
